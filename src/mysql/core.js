daf57cfc43d08f43b1007f9ac84d65e4a38b3efd
unknown <vankien9x@gmail.com>
2017-07-03 16:37:37 +0700

    db edit2

--- src/mysql/core.js
+++ src/mysql/core.js
@@ -5,8 +5,7 @@ var MYSQL = mysql.createConnection({
   host     : process.env.MYSQL_HOST,
   user     : process.env.MYSQL_USER,
   password : process.env.MYSQL_PWD,
-  database : process.env.MYSQL_DB,
-  port: process.env.MYSQL_PORT
+  database : process.env.MYSQL_DB
 });
 
 //code here
