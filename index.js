4cca1e037c662c0180db84a71e7756d67be15346
unknown <vankien9x@gmail.com>
2017-07-03 13:54:06 +0700

    fix again

--- index.js
+++ index.js
@@ -98,10 +98,7 @@ app.get('/', function(req, res){
 http.listen(process.env.PORT || 3000);
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+  
   socket.on('chat message', function(msg){
      io.emit('chat message', msg);
    });
